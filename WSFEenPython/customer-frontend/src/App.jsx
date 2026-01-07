22d22a4c2ffe28d8daffa1a5ceaf499da0f8dc4c
Danny-Ayuquina <dmayuquina@espe.edu.ec>
2026-01-07 14:43:29 -0500

    si

--- WSFEenPython/customer-frontend/src/App.jsx
+++ WSFEenPython/customer-frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
-  const API_URL = 'http://localhost:8000/oopdatabase/customers'
+  const API_URL = 'https://universidad-1i99.onrender.com/oopdatabase/customers'
 
   useEffect(() => {
     fetch(API_URL)
